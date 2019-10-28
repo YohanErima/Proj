71b7d02b80d938eb5a28d210f9842c7ed8f20709
yohan <etudiant@imac-de-admin-6.local>
2019-10-28 16:58:45 +0400

    first commit

---
+++ Proj/MapViewController.swift
@@ -0,0 +1,100 @@
+//
+//  MapViewController.swift
+//  Proj
+//
+//  Created by etudiant on 28/10/2019.
+//  Copyright © 2019 etudiant. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelegate {
+
+    @IBOutlet weak var MapVieew: MKMapView!
+    let locationManager = CLLocationManager()
+    var currentLocation: CLLocation?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+            
+            locationManager.requestWhenInUseAuthorization()
+            
+             MapVieew.showsUserLocation = true
+        }
+        else {
+            let alert = UIAlertController(title: NSLocalizedString("error", comment: "Error"),
+                                          message: NSLocalizedString("location not enabled", comment: "Error"),
+                                          preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    
+    
+    
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+    
+
+    
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        currentLocation = userLocation.location
+    }
+    
+    func mapView(_ mapView: MKMapView, didFailToLocateUserWithError error: Error) {
+        let alert = UIAlertController(title: NSLocalizedString("error", comment: "Error"),
+                                      message: NSLocalizedString("failed to update map", comment: "Error"),
+                                      preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        if annotation is MKUserLocation {
+            return nil
+        }
+        
+        let pin = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "dmi")
+        pin.pinTintColor = UIColor.red
+        pin.canShowCallout = true
+        pin.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+        return pin
+    }
+    
+    func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if let annonation = view.annotation {
+            var latitude = annonation.coordinate.latitude
+            var latitudeString = NSLocalizedString("north", comment: "Label")
+            if latitude < 0 {
+                latitude = -1 * latitude
+                latitudeString = NSLocalizedString("south", comment: "Label")
+            }
+            var longitude = annonation.coordinate.longitude
+            var longitudeString = NSLocalizedString("east", comment: "Label")
+            if longitude < 0 {
+                longitude = -1 * longitude
+                longitudeString = NSLocalizedString("west", comment: "Label")
+            }
+           
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+}
